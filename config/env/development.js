5eacea83b8a86ddf0af1302e514831f76d53a086
AL_SK <alexander.schackmann@googlemail.com>
2016-08-23 13:57:29 +0200

    First Draft

--- config/env/development.js
+++ config/env/development.js
@@ -4,10 +4,10 @@ var defaultEnvConfig = require('./default');
 
 module.exports = {
   db: {
-    uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mean-dev',
+    uri: 'mongodb://ds013486.mlab.com:13486/meandb',
     options: {
-      user: '',
-      pass: ''
+      user: 'admin',
+      pass: 'pass'
     },
     // Enable mongoose debug mode
     debug: process.env.MONGODB_DEBUG || false
--- modules/core/client/controllers/home.client.controller.js
+++ modules/core/client/controllers/home.client.controller.js
@@ -4,5 +4,44 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
   function ($scope, Authentication) {
     // This provides Authentication context.
     $scope.authentication = Authentication;
+
+    $scope.alerts = [
+      {
+        icon:'glyphicon-home',
+        colour:'btn-primary btn-lg',
+        name:'Home',
+        description:'Benutzer: Admin'
+      },
+      {
+        icon:'glyphicon-th',
+        colour:'btn-primary btn-lg',
+        name:'Zeiterfassungsmatrix',
+        description:'Heutiges Datum: 20.10.15'
+      },
+      {
+        icon:'glyphicon-time',
+        colour:'btn-primary btn-lg',
+        name:'Zeiterfassungsliste',
+        description:'Aktuelle Uhrzeit: 09:20'
+      },
+      {
+        icon:'glyphicon-calendar',
+        colour:'btn-primary btn-lg',
+        name:'Kalenderansicht',
+        description:'Kalenderwoche: 35'
+      },
+      {
+        icon:'glyphicon-plane',
+        colour:'btn-primary btn-lg',
+        name:'Urlaubsanträge',
+        description:'Urlaubstage vorhanden: 2'
+      },
+      {
+        icon:'glyphicon-education',
+        colour:'btn-primary btn-lg',
+        name:'Schulungsanträge',
+        description:'Schulungstage beantragt: 3'
+      }
+    ];
   }
 ]);
