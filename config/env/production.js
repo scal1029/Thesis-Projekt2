b5894f5f7e6c1aa8d7c899e445dc2491bb6559fe
AL_SK <alexander.schackmann@googlemail.com>
2016-09-20 11:20:07 +0200

    First Draft 3

--- config/env/production.js
+++ config/env/production.js
@@ -8,10 +8,10 @@ module.exports = {
   },
   port: process.env.PORT || 8443,
   db: {
-    uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mean',
+    uri: 'mongodb://ds013486.mlab.com:13486/meandb',
     options: {
-      user: '',
-      pass: ''
+      user: 'admin',
+      pass: 'pass'
     },
     // Enable mongoose debug mode
     debug: process.env.MONGODB_DEBUG || false
